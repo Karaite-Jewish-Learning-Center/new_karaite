4e7cf2344e5302e80e0a9015a469dfce8fa1f02c
sandro_fernandes <Sandro.Fernandes@toptal.com>
2021-03-03 18:58:44 -0300

    Models for books, comments , comment Authors

---
+++ newkaraites/karaites/constants.py
@@ -0,0 +1,7 @@
+# constant for new karaites experiment
+
+
+LANGUAGES = [
+    ('En', ' English'),
+    ('He', 'Hebrew')
+]
--- newkaraites/karaites/models.py
+++ newkaraites/karaites/models.py
@@ -1,3 +1,99 @@
 from django.db import models
+from django.utils.translation import gettext as _
+from .constants import LANGUAGES
 
-# Create your models here.
+
+class Organization(models.Model):
+    """
+        Books order
+    """
+
+    book_title_en = models.CharField(max_length=100,
+                                     null=True,
+                                     verbose_name=_("Book title English"))
+
+    book_title_he = models.CharField(max_length=100,
+                                     null=True,
+                                     verbose_name=_("Book title Hebrew"))
+
+    order = models.IntegerField(default=0,
+                                db_index=True,
+                                verbose_name=_('Presentation order'))
+
+    def __str__(self):
+        return f"{self.book_title_en}"
+
+    class Meta:
+        verbose_name_plural = _("Organizations")
+        ordering = ['order']
+
+
+class CommentAuthor(models.Model):
+    """
+        Who made the comment
+    """
+
+    name = models.CharField(max_length=50)
+
+    history = models.TextField(null=True,
+                               verbose_name=_("History"))
+
+    def __str__(self):
+        return f"{self.name}"
+
+    class Meta:
+        verbose_name_plural = "Comment Author"
+
+
+class Comment(models.Model):
+    """
+       Comments on chapter / verse
+    """
+
+    comment = models.TextField(null=True,
+                               verbose_name=_("A comment"))
+
+    comment_language = models.CharField(max_length=2,
+                                        choices=LANGUAGES,
+                                        verbose_name=_('Comment language'))
+
+    comment_author = models.ForeignKey(CommentAuthor,
+                                       verbose_name=_('Who comment'))
+
+    def __str__(self):
+        return f"{self.comment_author}"
+
+    class Meta:
+        verbose_name_plural = "Comments"
+
+
+class BookText(models.Model):
+    """
+       A bible book text
+    """
+
+    book = models.ForeignKey(Organization,
+                             on_delete=models.CASCADE,
+                             verbose_name=_('Book'))
+
+    chapter = models.IntegerField(default=0,
+                                  verbose_name=_("Chapter"))
+
+    verse = models.IntegerField(default=0,
+                                verbose_name=_("Verse"))
+
+    text_en = models.TextField(null=True,
+                               verbose_name=_("English chapter Verse text"))
+
+    text_he = models.TextField(null=True,
+                               verbose_name=_("Hebrew chapter Verse text"))
+
+    comments = models.ManyToManyField(Comment)
+
+    def __str__(self):
+        return f"{self.book.book_title_en}"
+
+    class Meta:
+        verbose_name_plural = _("Book text")
+
+
