6cc26374dcf2e60705e18d831f0445929dfd0b52
sandro_fernandes <Sandro.Fernandes@toptal.com>
2021-04-29 18:41:05 -0300

    Parsing html some insights that may simplify things.

--- newkaraites/karaites/management/commands/comments.py
+++ newkaraites/karaites/management/commands/comments.py
@@ -9,32 +9,85 @@ from bs4 import BeautifulSoup
 class Command(BaseCommand):
     help = 'Populate Database with comments'
 
+    # @staticmethod
+    # def get_chapter_verse(html):
+    #     """ Parse chapter number and verse."""
+    #     chapter = None
+    #     verse = None
+    #     # most of times span follows <p class="MsoNormal"..
+    #     span = html.find('span')
+    #     if span is None:
+    #         # some times <b> encapsulates <span> that has some text
+    #         span = html.find('b').find('span')
+    #
+    #     if span is not None:
+    #         try:
+    #             text = span.text.replace(' ', '')
+    #             # 11:9-11:10
+    #             pattern = re.search('[0-9]*:[0-9]*–[0-9]*:[0-9]*', text)
+    #
+    #             if pattern is not None and pattern.start() == 0:
+    #                 parts = pattern.group().split(':')
+    #                 chapter = parts[0]
+    #                 start = parts[1][:parts[1].find("–")]
+    #                 end = parts[2]
+    #                 verse = list(map(str, range(int(start), int(end)+1)))
+    #                 print(chapter, verse, 'first')
+    #                 return chapter, verse
+    #
+    #             # 11:9 , 11:9-11 -> 11:9 , 11:10, 11:11
+    #             pattern = re.search('[0-9,:,–]+', text)
+    #
+    #             if pattern is not None and pattern.start() == 0:
+    #                 chapter, possible_range = pattern.group().split(":")
+    #
+    #                 if possible_range.find('–') > 0:
+    #                     start, end = possible_range.split('–')
+    #                     verse = list(map(str, range(int(start), int(end)+1)))
+    #                 else:
+    #                     verse = [possible_range]
+    #
+    #                 print(chapter, verse, 'second')
+    #                 return chapter, verse
+    #
+    #         except (ValueError, IndexError):
+    #             pass
+    #     return chapter, verse
+
     @staticmethod
     def get_chapter_verse(html):
-        """ Parse chapter number"""
+        """ Parse chapter number and verse."""
         chapter = None
         verse = None
-        # most of times span follows <p class="MsoNormal"..
-        span = html.find('span')
-        if span is None:
-            # some times <b> encapsulates <span> that has some text
-            span = html.find('b').find('span')
-
-        if span is not None:
+        if html is not None:
             try:
+                text = html.text.replace(' ', '')
                 # 11:9-11:10
-                pattern = re.search('[0-9]*:[0-9]*–[0-9]*:[0-9]*', span.text)
+                pattern = re.search('[0-9]*:[0-9]*–[0-9]*:[0-9]*', text)
+
                 if pattern is not None and pattern.start() == 0:
                     parts = pattern.group().split(':')
                     chapter = parts[0]
-                    verse = parts[1][:parts[1].find("–")+1]+parts[2]
-                    print(chapter, verse)
+                    start = parts[1][:parts[1].find("–")]
+                    end = parts[2]
+                    verse = list(map(str, range(int(start), int(end) + 1)))
+                    print(chapter, verse, 'first')
                     return chapter, verse
 
-                # 11:9 , 11:9-10
-                pattern = re.search('[0-9,:,–]+', span.text)
+                # 11:9 , 11:9-11 -> 11:9 , 11:10, 11:11
+                pattern = re.search('[0-9,:,–]+', text)
+
                 if pattern is not None and pattern.start() == 0:
-                    return pattern.group().split(" ")[0].split(":")
+                    chapter, possible_range = pattern.group().split(":")
+
+                    if possible_range.find('–') > 0:
+                        start, end = possible_range.split('–')
+                        verse = list(map(str, range(int(start), int(end) + 1)))
+                    else:
+                        verse = [possible_range]
+
+                    print(chapter, verse, 'second')
+                    return chapter, verse
 
             except (ValueError, IndexError):
                 pass
@@ -60,7 +113,7 @@ class Command(BaseCommand):
                         chapter, verses = same_chapter, same_verses
 
                     chapter_number = int(chapter)
-                    for verse in verses.split("–"):
+                    for verse in verses:
                         verse_number = int(verse)
                         try:
                             comment = Comment.objects.get(book=organization, chapter=chapter_number, verse=verse_number)
