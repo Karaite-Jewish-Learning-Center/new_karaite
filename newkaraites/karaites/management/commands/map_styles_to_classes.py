0ff317c27ee7be101c269e7a01e326dfada8ce95
sandro_fernandes <Sandro.Fernandes@toptal.com>
2021-12-04 19:31:46 -0300

    Made some progress parsing html books. Reduced size from 3 megabytes to less them 500 kb. Collect all inline styles to css files with less them 50 kb.

--- newkaraites/karaites/management/commands/map_styles_to_classes.py
+++ newkaraites/karaites/management/commands/map_styles_to_classes.py
@@ -6,97 +6,86 @@ style_classes = {}
 ms_classes = []
 tags = {}
 
-#
-# class Command(BaseCommand):
-#     help = 'Populate Database with Karaites books as array at this point is only for the books bellow'
-new_html = []
 
+class Command(BaseCommand):
+    help = 'Populate Database with Karaites books as array at this point is only for the books bellow'
 
-def recurse(element):
-    if hasattr(element, 'children'):
-        for child in element.children:
-            if hasattr(child, 'attrs'):
-                style = child.attrs.get('style', None)
-                tag = child.name
-                class_name = None
-                if tag not in tags:
-                    tags[tag] = 0
+    def handle(self, *args, **options):
+        """ Karaites books as array """
+        # new_html = BeautifulSoup('<div id="start"></div>', 'html5lib')
 
-                if style is not None:
-                    # print()
-                    # print(style)
-                    # add a style style_classes
-                    style = style.strip().replace(' ', '').replace('\n', '').replace('\r', '')
-                    if style not in style_classes:
-                        class_name = f'{child.name}-{tags[tag]}'
-                        style_classes.update({style: class_name})
-                        child.attrs.pop('style')
-                        tags[tag] += 1
+        for volume in [1, 2]:
+            # source = (f'../newkaraites/data_karaites/Shelomo_Afeida_HaKohen_Yeriot_Shelomo/'
+            source = (
+                f'/Users/sandro/anaconda3/envs/kjoa/new_karaite/newkaraites/data_karaites/Shelomo_Afeida_HaKohen_Yeriot_Shelomo/'
+                f'Shelomo Afeida HaKohen_Yeriot Shelomo_Volume {volume}.html')
 
-                    if child.attrs.get('class', None) is not None:
-                        ms_class = child.attrs['class']
-                        if ms_class not in ms_classes:
-                            ms_classes.append(ms_class)
-                        if class_name is not None:
-                            child.attrs['class'].append(class_name)
-                    else:
-                        if class_name is not None:
-                            child.attrs['class'] = [class_name]
-            recurse(child)
-    new_html.append(element)
-    return element
+            out = (f'/Users/sandro/anaconda3/envs/kjoa/new_karaite/newkaraites/karaites/management/tmp/'
+                   f'Shelomo Afeida HaKohen_Yeriot Shelomo_Volume {volume}.html')
 
+            css = f'/Users/sandro/anaconda3/envs/kjoa/new_karaite/newkaraites/karaites/management/tmp/tmp-{volume}.css'
+            ms_css = f'../newkaraites/tmp/Shelomo_Afeida_HaKohen_Yeriot_Shelomo/ms-tmp.css'
 
-def handle():
-    """ Karaites books as array """
-    # new_html = BeautifulSoup('<div id="start"></div>', 'html5lib')
-    for volume in [1]:  # , 2]:
-        # source = (f'../newkaraites/data_karaites/Shelomo_Afeida_HaKohen_Yeriot_Shelomo/'
-        source = (f'/Users/sandro/anaconda3/envs/kjoa/new_karaite/newkaraites/data_karaites/Shelomo_Afeida_HaKohen_Yeriot_Shelomo/'
-                  f'Shelomo Afeida HaKohen_Yeriot Shelomo_Volume {volume}.html')
+            handle_source = open(source, 'r')
+            html = handle_source.read()
+            handle_source.close()
 
-        out = (f'/Users/sandro/anaconda3/envs/kjoa/new_karaite/newkaraites/karaites/management/tmp/'
-               f'Shelomo Afeida HaKohen_Yeriot Shelomo_Volume {volume}.html')
+            html = html.replace('<o:p></o:p>', '').replace(r'<o:p>', '').replace(r'</o:p>', '')
 
-        css = f'/Users/sandro/anaconda3/envs/kjoa/new_karaite/newkaraites/karaites/management/tmp/tmp-{volume}.css'
-        ms_css = f'../newkaraites/tmp/Shelomo_Afeida_HaKohen_Yeriot_Shelomo/ms-tmp.css'
+            html_tree = BeautifulSoup(html, 'html5lib', )
 
-        handle_source = open(source, 'r')
-        html_tree = BeautifulSoup(handle_source.read(), 'html5lib')
-        handle_source.close()
-        divs = html_tree.find_all('div', class_="WordSection1", recursive=True)
-        children = divs[0].find_all(recursive=True)
+            divs = html_tree.find('div', class_="WordSection1", recursive=True)
 
-        i = 1
-        for child in children:
-            recurse(child)
-            new_html.reverse()
-            print(new_html)
+            i = 1
+            for tag in divs.findAll(True):
+                for attr in [attr for attr in tag.attrs]:
 
-            # print('After ' + '-' * 90)
-            # print(child)
-            # input('>>')
-            # i += 1
-            # if i > 2:
-            break
-            sys.stdout.write(f"\33[K Processing volume {volume} line: {i}\r")
-        sys.stdout.write(f"\33[K Writing files\r")
-        handle_out = open(out, 'w')
-        handle_out.write(str(children))
-        handle_out.close()
+                    # remove this attrs keep all other's
+                    if attr in ['dir', 'lang']:
+                        del tag[attr]
+                        continue
 
-        handle_css = open(css, 'w')
+                    # remove local style , replace by a class
+                    if attr == 'style':
+                        class_name = None
+                        style = tag.attrs['style']
+                        if tag.name not in tags:
+                            tags[tag.name] = 0
 
-        for style, class_name in style_classes.items():
-            # line_breaks = style.replace(";", ";\n   ")
-            handle_css.write(f'.{class_name} {{\n   {style}\n}}\n')
-        handle_css.close()
+                        style = style.strip().replace(' ', '').replace('\n', '').replace('\r', '')
+                        if style not in style_classes:
+                            class_name = f'{tag.name}-{tags[tag.name]}'
+                            style_classes.update({style: class_name})
+                            tags[tag.name] += 1
 
-        # handle_ms_css = open(ms_css, 'w')
-        # for class_name in ms_class:
-        #     line_breaks = style.repla
-        #     handle_ms_css.write(f'.{class_name} {{\n   {line_breaks}\n}}\n')
-        # handle_ms_css.close()
+                        del tag[attr]
 
+                        if tag.attrs.get('class', None) is not None:
+                            ms_class = tag.attrs['class']
+                            if ms_class not in ms_classes:
+                                ms_classes.append(ms_class)
+                            if class_name is not None:
+                                tag.attrs['class'].append(class_name)
+                        else:
+                            if class_name is not None:
+                                tag.attrs['class'] = [class_name]
+                i += 1
+                sys.stdout.write(f"\33[K Processing volume {volume} line: {i}\r")
 
-handle()
+            sys.stdout.write(f"\33[K Writing files\r")
+
+            handle_out = open(out, 'w')
+            handle_out.write(str(divs))
+            handle_out.close()
+
+            handle_css = open(css, 'w')
+            for style, class_name in style_classes.items():
+                # line_breaks = style.replace(";", ";\n   ")
+                handle_css.write(f'.{class_name} {{\n   {style}\n}}\n')
+            handle_css.close()
+
+            # handle_ms_css = open(ms_css, 'w')
+            # for class_name in ms_class:
+            #     line_breaks = style.replace
+            #     handle_ms_css.write(f'.{class_name} {{\n   {line_breaks}\n}}\n')
+            # handle_ms_css.close()
