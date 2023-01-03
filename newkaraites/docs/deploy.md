a6e8ca35e14096aaa810c84c9a5230114ab1394d
sandro_fernandes <Sandro.Fernandes@toptal.com>
2023-01-03 15:42:53 -0300

    Clean up and add LICENSE

--- newkaraites/docs/deploy.md
+++ newkaraites/docs/deploy.md
@@ -3,9 +3,9 @@ Out dated, for reference only.
 []: # Language: markdown
 []: # Path: newkaraites/docs/deploy.md
 # Deploy
-2) the usual stuff git pull, ./manager migrate, collect .....
-3) pip requirements
-4) python -m spacy download en_core_web_sm
-5) create autocomplete_view in autocomplete table (see SQL directory)
-6) Create index on fulltextsearch (see SQL/index directory)
+1) the usual stuff git pull, ./manager migrate, collect .....
+2) pip requirements
+3) python -m spacy download en_core_web_sm
+4) create autocomplete_view in autocomplete table (see SQL directory)
+5) Create index on fulltextsearch (see SQL/index directory)
 
--- newkaraites/karaites/urls.py
+++ newkaraites/karaites/urls.py
@@ -22,8 +22,6 @@ urlpatterns = [
 
     # book list
 
-    # deprecated
-    # path('books-list/', BooksPresentation.as_view(), name='books_list'),
 
     path('get-book-chapter/<str:book>/<str:chapter>/<int:first>/',
          GetBookAsArrayJson.as_view(), name='get_book_chapter'),
@@ -31,14 +29,12 @@ urlpatterns = [
     # audiobooks
     path('audio-book/<str:book>/', AudioBook.as_view(), name='audio_book'),
 
-    # path('get-book-chapter/<str:book>/', GetBookAsArrayJson.as_view(), name='get_book_chapter'),
 
     # karaite books
     path('get-karaites-book-chapter/<str:book>/<str:chapter>/<int:first>/',
          GetKaraitesBookAsArray.as_view(),
          name='get_karaites_book_chapter'),
 
-    # path('get-karaites-book/<str:book>/', GetKaraitesBookAsArray.as_view(), name='get_karaites_book'),
     path('get-karaites-book-details/', GetKaraitesAllBookDetails.as_view(), name='get_all_karaites_book_details'),
     path('get-karaites-book-toc/<str:book>/', GetTOC.as_view(), name='get_karaites_book_toc'),
 
