028bac58b5d5c39b3285170a5cf30d6a7636b46c
SandroFernandes <sandro.fernandes@softrent.com.br>
2023-04-12 17:42:30 -0300

    Exporting to excel command.

--- newkaraites/karaites/management/commands/export_to_excel.py
+++ newkaraites/karaites/management/commands/export_to_excel.py
@@ -1,13 +1,22 @@
 import sys
 from bs4 import BeautifulSoup
+from bidi.algorithm import get_display
 from django.core.management.base import BaseCommand
 from ...models import KaraitesBookAsArray
 from openpyxl import Workbook
+from openpyxl.styles import Font
 from pathlib import Path
 from .command_utils.argments import arguments
 from .process_arguments import process_arguments
 
 file_name = Path().cwd() / 'exported/'
+
+hebrew_script = {"script": "Hebrew"}
+
+
+class HebrewScript():
+    tagname = "rtl"
+    values = ('Hebrew',)
 
 
 class Command(BaseCommand):
@@ -19,7 +28,6 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         """Export book from database to excel"""
         query = process_arguments(options)
-        print(query)
         if not query:
             return
 
@@ -29,6 +37,7 @@ class Command(BaseCommand):
             wb = Workbook()
             ws1 = wb.active
             ws1.title = title
+            ws1.column_dimensions['A'].width = 15
 
             ws1[f'A{row}'] = 'Paragraph'
             ws1[f'B{row}'] = 'Hebrew'
@@ -44,12 +53,20 @@ class Command(BaseCommand):
                 html_en = book.book_text[0]
                 soup_he = BeautifulSoup(html_he, 'html.parser')
                 soup_en = BeautifulSoup(html_en, 'html.parser')
+
+                foot_notes_he = ''
+                for foot_note in soup_he.find_all('span', {'class': "en-foot-note"}):
+                    foot_notes_he += foot_note.get('data-tip').strip().replace('\n', '').replace('\xa0', '') + '\n'
+
                 paragraphs_number = book.paragraph_number
 
                 ws1[f'A{row}'] = paragraphs_number
                 ws1[f'B{row}'] = soup_he.get_text()
                 ws1[f'C{row}'] = soup_en.get_text()
-                # ws1[f'D{row}'] = book.foot_notes[0]
+                ws1[f'D{row}'] = foot_notes_he
+
                 row += 1
-                sys.stdout.write(f'\rProcessing book:{book.book.book_title_en}  paragraph:{book.paragraph_number}       ')
+                sys.stdout.write(
+                    f'\rProcessing book:{book.book.book_title_en}  paragraph:{book.paragraph_number}       ')
+                sys.stdout.flush()
             wb.save(file_name / f'{title}.xlsx')
