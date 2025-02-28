9f9e6c889f53be460f8e94fba354eab21ce86f71
SandroFernandes <sandro.fernandes@softrent.com.br>
2025-02-28 13:45:53 -0300

    Cache enabled

--- newkaraites/settings.py
+++ newkaraites/settings.py
@@ -251,15 +251,15 @@ LOGGING = {
     },
 }
 
-# CACHES = {
-#     'default': {
-#         'BACKEND': 'django_redis.cache.RedisCache',
-#         'LOCATION': 'redis://redis:6379/1',
-#         'OPTIONS': {
-#             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
-#         }
-#     }
-# }
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://redis:6379/1',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
+    }
+}
 
 # Cache time to live is 1 day
 CACHE_TTL = 60 * 60 * 24
